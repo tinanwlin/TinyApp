83ee3ada42a62405b660b20d7cdc47c33c995c1a
tinanwlin <tinanwlin@gmail.com>
2018-02-22 05:23:50 +0000

    Create a app with homepage, can add new url, update url and delete url.

---
+++ express_server.js
@@ -0,0 +1,103 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));  //ORDER????
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+//new add for view the template OK
+app.get("/urls", (req, res) => {
+  console.log('/urls get:')
+  let templateVars = { urls: urlDatabase };  // temp = { 
+    console.log(templateVars)
+  res.render("urls_index", templateVars);  //         urls: {"key": "value"} }
+});
+
+//2nd route post
+app.get("/urls/new", (req, res) => {
+  console.log("get:")
+
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log("post:")
+  var generateFun = generateRandomString();
+  urlDatabase[generateFun] = req.body.longURL;
+  console.log('newURL', req.body);  
+  console.log('newURL', req.body.longURL);
+  console.log(urlDatabase);
+  console.log("redirect:")
+  res.redirect("urls/" + generateFun);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log("/u/get:")
+  console.log(req.params);
+
+  let longURL = urlDatabase[req.params.shortURL];
+
+  console.log('database', urlDatabase)
+  console.log(longURL)
+  res.redirect(longURL);
+});
+
+//new add for shortened form OK
+app.get("/urls/:id", (req, res) => {
+  console.log("url/ID")
+  console.log(req.params)
+  let templateVars = {
+    shortURL: req.params.id, //req.params = {id(from:id): "2222"} 2222
+    longURL: urlDatabase[req.params.id] //yahoo.com
+  };
+  console.log(templateVars);
+  res.render("urls_show", templateVars); //show the data into urls_show
+});
+
+
+//delete a shortURL
+app.post("/urls/:id/delete", (req, res) => {
+  console.log("delete")
+  console.log(req.params)
+  var deleteshortURL = req.params.id; 
+  delete urlDatabase[deleteshortURL];
+  res.redirect("/urls");
+});
+
+
+//update URL
+app.post("/urls/:id", (req, res) => {
+  console.log("update")
+  console.log("params:", req.params)
+  console.log("body:", req.body)
+  var updateURL = req.params.id; 
+  urlDatabase[updateURL] = req.body.shortURL;
+  console.log(updateURL);
+  res.redirect("/urls");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
